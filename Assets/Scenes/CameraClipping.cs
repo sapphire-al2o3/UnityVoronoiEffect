22a75721168f2cc311a4b71594d8c56bf25b2538
sapphire-al2o3 <t_telkun@hotmail.com>
2021-11-06 23:18:17 +0900

    マテリアル名変更

---
+++ Assets/Scenes/CameraClipping.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class CameraClipping : MonoBehaviour
+{
+	void Awake()
+    {
+
+
+		var commandBuffer = new CommandBuffer();
+		commandBuffer.name = "Clipping";
+		commandBuffer.EnableScissorRect(new Rect(0, 0, 0, 0));
+		//commandBuffer.SetViewport(new Rect(0, 0, 100, 100));
+		var camera = GetComponent<Camera>();
+
+		//var material = new Material(Shader.Find("Standard"));
+		//commandBuffer.DrawMesh(_mesh, Matrix4x4.identity, material, 0, 0);
+
+		camera.AddCommandBuffer(CameraEvent.BeforeForwardOpaque, commandBuffer);
+
+		Debug.Log(camera.commandBufferCount);
+	}
+}
